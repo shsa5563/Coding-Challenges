0c192d3cfe71addeef8d855180fc4e7424814e2b
Shekhar H S <shsa5563@colorado.edu>
2018-05-05 21:57:26 -0600

    Create jewels and Stones.c

---
+++ LeetCode/jewels and Stones.c
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int numJewelsInStones(string J, string S) {
+        unordered_set<char, int> temp; 
+        for(int i=0; i<S.size() ; i++) ++temp[S[i]; 
+        for( auto i:J)
+        {
+            if()
+        }
+        
+    }
+};
